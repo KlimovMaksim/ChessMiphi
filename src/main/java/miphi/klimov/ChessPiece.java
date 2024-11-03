cb51dc33778f7f20e21918c308c3ee4e3c2400bd
Maksim Klimov <maksimklimov779@gmail.com>
2024-11-03 18:31:29 +0300

    feat: add ChessPiece

---
+++ src/main/java/miphi/klimov/ChessPiece.java
@@ -0,0 +1,19 @@
+package miphi.klimov;
+
+public abstract class ChessPiece {
+
+    String color;
+    boolean check;
+
+    public ChessPiece(String colour) {
+        this.color = colour;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    public abstract String getSymbol();
+}
