9ada8b20b27f34e7a4564382771c29bc5d187940
Maksim Klimov <maksimklimov779@gmail.com>
2024-11-03 19:11:37 +0300

    feat: add Pawn

--- src/main/java/miphi/klimov/Pawn.java
+++ src/main/java/miphi/klimov/Pawn.java
@@ -1,4 +1,48 @@
 package miphi.klimov;
 
-public class Pawn {
+public class Pawn extends ChessPiece {
+
+    public Pawn(String colour) {
+        super(colour);
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        // check end position is within the chessboard
+        if (!chessBoard.checkPos(toLine) || !chessBoard.checkPos(toColumn)) return false;
+
+        // check end and start position are different
+        if (line == toLine && column == toColumn) return false;
+
+        // check pawn moves only vertically
+        if (column != toColumn) return false;
+
+        if (color.equals("White")) {
+            // first move is 2 cells forward
+            if (line == 1 && toLine == line + 2) return true;
+
+            // usual move is 1 cell forward
+            if (toLine == line + 1) return true;
+        }
+
+        if (color.equals("Black")) {
+            // first move is 2 cells forward
+            if (line == 6 && toLine == line - 2) return true;
+
+            // usual move is 1 cell forward
+            if (toLine == line - 1) return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
 }
