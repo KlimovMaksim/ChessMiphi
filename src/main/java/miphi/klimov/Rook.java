f8753a80270839512967126b09f5bb334706da12
Maksim Klimov <maksimklimov779@gmail.com>
2024-11-07 17:30:42 +0300

    feat: adde Rook

--- src/main/java/miphi/klimov/Rook.java
+++ src/main/java/miphi/klimov/Rook.java
@@ -1,4 +1,30 @@
 package miphi.klimov;
 
-public class Rook {
+public class Rook extends ChessPiece {
+
+    public Rook(String colour) {
+        super(colour);
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        // check end position is within the chessboard
+        if (!chessBoard.checkPos(toLine) || !chessBoard.checkPos(toColumn)) return false;
+
+        // check end and start position are different
+        if (line == toLine && column == toColumn) return false;
+
+        // check movement complies with the Rook rules
+        return !(line != toLine && column != toColumn);
+    }
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
 }
