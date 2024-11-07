6b41fcf9504cdd466c7964bf7630632fa3e3bc73
Maksim Klimov <maksimklimov779@gmail.com>
2024-11-07 16:36:33 +0300

    feat: add Bishop

---
+++ src/main/java/miphi/klimov/Bishop.java
@@ -0,0 +1,32 @@
+package miphi.klimov;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(String colour) {
+        super(colour);
+    }
+
+    @Override
+    public String getColor() {
+        return super.getColor();
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        // check end position is within the chessboard
+        if (!chessBoard.checkPos(toLine) || !chessBoard.checkPos(toColumn)) return false;
+
+        // check end and start position are different
+        if (line == toLine && column == toColumn) return false;
+
+        int deltaX = Math.abs(column - toColumn);
+        int deltaY = Math.abs(line - toLine);
+
+        return deltaX == deltaY;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "B";
+    }
+}
